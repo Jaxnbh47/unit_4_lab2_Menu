61f45229ca6f8c4c80d6b46d6105773e7b553bbe
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-11-02 21:48:42 -0400

    master

---
+++ menu.js
@@ -0,0 +1,15 @@
+function main2() {
+    let hideme = document.getElementById("hideme")
+    hideme.style.display = "block";
+
+    let showme = document.getElementById("showme")
+    showme.style.display = "none";
+}
+
+function main1() {
+    let hideme = document.getElementById("hideme")
+    hideme.style.display = "none";
+
+    let showme = document.getElementById("showme")
+    showme.style.display = "block";
+}
